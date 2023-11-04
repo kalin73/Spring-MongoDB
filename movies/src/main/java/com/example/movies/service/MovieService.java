9d9dd6edea1fbfbd5be619fb40c0bbbed4d040ab
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-04 16:03:45 +0200

    commit

--- movies/src/main/java/com/example/movies/service/MovieService.java
+++ movies/src/main/java/com/example/movies/service/MovieService.java
@@ -3,6 +3,7 @@ package com.example.movies.service;
 import com.example.movies.model.entity.Movie;
 import com.example.movies.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,11 +13,15 @@ import java.util.List;
 public class MovieService {
     private final MovieRepository movieRepository;
 
-    public List<Movie> allMovies(){
+    public List<Movie> allMovies() {
         return this.movieRepository.findAll();
     }
 
     public void addMovie(Movie movie) {
         this.movieRepository.save(movie);
+    }
+
+    public Movie getMovieById(ObjectId id) {
+        return this.movieRepository.findById(id).get();
     }
 }
--- movies/src/main/java/com/example/movies/web/rest/MovieRestController.java
+++ movies/src/main/java/com/example/movies/web/rest/MovieRestController.java
@@ -3,10 +3,9 @@ package com.example.movies.web.rest;
 import com.example.movies.model.entity.Movie;
 import com.example.movies.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,11 @@ public class MovieRestController {
     @GetMapping("/all")
     public ResponseEntity<List<Movie>> getAllMovies() {
         return ResponseEntity.ok(this.movieService.allMovies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovie(@PathVariable("id") ObjectId id) {
+        return ResponseEntity.ok(this.movieService.getMovieById(id));
     }
 
 }
