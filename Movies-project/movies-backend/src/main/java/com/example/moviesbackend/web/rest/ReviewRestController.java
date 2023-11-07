550aa5a56e5beede63768538893ee352ead3c303
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-07 19:55:43 +0200

    Changing reviews api endpoint

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/web/rest/ReviewRestController.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/web/rest/ReviewRestController.java
@@ -5,6 +5,7 @@ import com.example.moviesbackend.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewRestController {
     private final ReviewService reviewService;
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<Review> create(@RequestBody Map<String, String> payload) {
         return ResponseEntity.created(URI.create("")).body(this.reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")));
     }
