c01eb45805d060c85e63b184caf917d7ba177f39
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-07 13:51:15 +0200

    removing project

--- Movies-project/movies-backend/src/main/java/com/example/movies/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.movies.repository;
-
-import com.example.movies.model.entity.Review;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
-}
