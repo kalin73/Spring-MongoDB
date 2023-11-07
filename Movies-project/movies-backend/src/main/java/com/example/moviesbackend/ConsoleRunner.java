afe4a16e93bdd79e3b7910eeed3d08dcf9cce60e
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-07 19:19:34 +0200

    commit

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/ConsoleRunner.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/ConsoleRunner.java
@@ -18,16 +18,16 @@ public class ConsoleRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Movie movie = new Movie();
-        movie.setImdbId("qwe");
-        movie.setTitle("Fast and Furious 8");
-        movie.setReleaseDate("14.04.2017");
-        movie.setTrailerLink("https://www.youtube.com/watch?v=uisBaTkQAEs&ab_channel=UniversalPicturesUK");
-        movie.setPoster("./image/FF8.jpg");
-        movie.setGenres(List.of("Action"));
-        movie.setBackdrops(List.of());
-
-
-        this.movieRepository.save(movie);
+//        Movie movie = new Movie();
+//        movie.setImdbId("qwe");
+//        movie.setTitle("Fast and Furious 8");
+//        movie.setReleaseDate("14.04.2017");
+//        movie.setTrailerLink("https://www.youtube.com/watch?v=uisBaTkQAEs&ab_channel=UniversalPicturesUK");
+//        movie.setPoster("./image/FF8.jpg");
+//        movie.setGenres(List.of("Action"));
+//        movie.setBackdrops(List.of());
+//
+//
+//        this.movieRepository.save(movie);
     }
 }
