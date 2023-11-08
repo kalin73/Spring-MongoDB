67a27fe5951fa3e73b502254a8451446d6b9f2e7
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-08 21:24:14 +0200

    commit

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/config/BeanConfiguration.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/config/BeanConfiguration.java
@@ -3,12 +3,20 @@ package com.example.moviesbackend.config;
 import com.example.moviesbackend.utils.LoggedUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.context.annotation.SessionScope;
 
 @Configuration
 public class BeanConfiguration {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/api/**").permitAll());
+
+        return httpSecurity.build();
+    }
     @Bean
     @SessionScope
     LoggedUser loggedUser(){
