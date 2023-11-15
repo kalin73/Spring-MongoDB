1db897a8961f90211d204989a1f9c1a51c9e5423
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-15 17:13:08 +0200

    Adding new constant

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/utils/Constants.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/utils/Constants.java
@@ -3,4 +3,5 @@ package com.example.moviesbackend.utils;
 public class Constants {
     public static final String DEFAULT_EMAIL = "anonymous@gmail.com";
     public static final String DEFAULT_USERNAME = "Anonymous";
+    public static final String FRONT_END_DOMAIN_URL = "http://localhost:3000/";
 }
