44ca89f9d3e0558a1433a8993bfeb1de463b8937
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-23 23:38:36 +0200

    adding caching

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/service/MovieService.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/service/MovieService.java
@@ -3,6 +3,8 @@ package com.example.moviesbackend.service;
 import com.example.moviesbackend.model.dto.MovieDto;
 import com.example.moviesbackend.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +14,14 @@ import java.util.List;
 public class MovieService {
     private final MovieRepository movieRepository;
 
+    @Cacheable(cacheNames = "movies")
     public List<MovieDto> allMovies() {
         return this.movieRepository.findAll().stream().map(MovieDto::mapToMovieDto).toList();
+    }
+
+    @CacheEvict(cacheNames = "movies", allEntries = true)
+    public void refreshMovies() {
+
     }
 
     public MovieDto getMovieByImdbId(String imdbId) {
