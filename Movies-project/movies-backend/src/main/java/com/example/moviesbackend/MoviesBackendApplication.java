54add18ae375ecf9a287eb6f8dd92c4a013c24a4
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-23 23:35:10 +0200

    enabling caching

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/MoviesBackendApplication.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/MoviesBackendApplication.java
@@ -2,10 +2,12 @@ package com.example.moviesbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class MoviesBackendApplication {
 
     public static void main(String[] args) {
