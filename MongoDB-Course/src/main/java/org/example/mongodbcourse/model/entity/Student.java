11b0ea8561891f97ab525159f621187114411048
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 01:15:35 +0300

    optimizing imports

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Student.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Student.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.mongodbcourse.model.enums.Gender;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @Document(collection = "students")
