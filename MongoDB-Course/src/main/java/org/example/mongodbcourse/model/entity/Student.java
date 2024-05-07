48d1ae82104f85bda5d6296974b18f4b67967948
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 01:01:49 +0300

    adding constructors

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Student.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Student.java
@@ -1,23 +1,28 @@
 package org.example.mongodbcourse.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.mongodbcourse.model.enums.Gender;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Document(collection = "students")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student extends BaseDocument {
     private String firstName;
     private String lastName;
     private String email;
     private Gender gender;
     private Address address;
-    private ZonedDateTime createdAt;
+    private LocalDateTime createdAt;
     private List<String> favouriteSubjects;
     private BigDecimal totalSpentInBooks;
 }
