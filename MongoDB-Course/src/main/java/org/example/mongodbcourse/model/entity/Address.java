a7d899492f5f4ff6b9b42123741d926a9fd66cbb
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 01:09:27 +0300

    fix

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Address.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Address.java
@@ -3,13 +3,11 @@ package org.example.mongodbcourse.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "addresses")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Address extends BaseDocument {
+public class Address {
     private String country;
     private String city;
     private String postCode;
