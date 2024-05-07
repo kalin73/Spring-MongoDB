0f4ca09732abede6ee553b3ac1b64ba3f6ec5cd9
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-07 12:53:00 +0300

    init commit

---
+++ MongoDB-Course/src/test/java/org/example/mongodbcourse/MongoDbCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mongodbcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
