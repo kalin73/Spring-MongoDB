151a703ab428f3c7ef41f99106a9c9dbd837fb7d
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 01:11:07 +0300

    test

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/MongoDbCourseApplication.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/MongoDbCourseApplication.java
@@ -1,7 +1,17 @@
 package org.example.mongodbcourse;
 
+import org.example.mongodbcourse.model.entity.Address;
+import org.example.mongodbcourse.model.entity.Student;
+import org.example.mongodbcourse.model.enums.Gender;
+import org.example.mongodbcourse.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class MongoDbCourseApplication {
@@ -10,4 +20,21 @@ public class MongoDbCourseApplication {
         SpringApplication.run(MongoDbCourseApplication.class, args);
     }
 
+
+    @Bean
+    CommandLineRunner runner(StudentRepository repository) {
+        return a -> {
+            Address address = new Address("Bulgaria", "Sofia", "1000");
+            Student student = new Student("Pesho",
+                    "Ivanov",
+                    "pesho@abv.bg",
+                    Gender.MALE,
+                    address,
+                    LocalDateTime.now(),
+                    List.of("Math", "Computer Science"),
+                    BigDecimal.TEN
+            );
+            repository.save(student);
+        };
+    }
 }
