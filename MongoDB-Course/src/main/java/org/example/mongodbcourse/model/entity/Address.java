c4f45db9bc66a83b7b0532c554ea7a497591bcf0
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 01:03:55 +0300

    adding constructors

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Address.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/model/entity/Address.java
@@ -1,10 +1,14 @@
 package org.example.mongodbcourse.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "addresses")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address extends BaseDocument {
     private String country;
     private String city;
