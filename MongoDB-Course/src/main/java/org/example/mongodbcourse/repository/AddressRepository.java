d8bfdf7b4f8c784c2771e39b95a445bce8e5f477
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 01:10:03 +0300

    removing unnecessary code

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.mongodbcourse.repository;
-
-import org.example.mongodbcourse.model.entity.Address;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AddressRepository extends MongoRepository<Address, String> {
-}
