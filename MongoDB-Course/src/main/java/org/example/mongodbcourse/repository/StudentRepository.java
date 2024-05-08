79fcba5a144f2f1d36f967feef7901770722976a
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-08 11:21:09 +0300

    adding method

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/repository/StudentRepository.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/repository/StudentRepository.java
@@ -3,5 +3,8 @@ package org.example.mongodbcourse.repository;
 import org.example.mongodbcourse.model.entity.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends MongoRepository<Student, String> {
+    Optional<Student> findStudentByEmail(String email);
 }
