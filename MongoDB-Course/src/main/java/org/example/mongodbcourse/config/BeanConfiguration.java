5125eaa0d089bd6d059985c3ea636315f56d11a7
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-12 23:33:58 +0300

    adding model mapper bean

---
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/config/BeanConfiguration.java
@@ -0,0 +1,13 @@
+package org.example.mongodbcourse.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
