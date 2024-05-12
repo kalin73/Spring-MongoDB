f8bb924d9a4d8f12bc18c9f98888b47423322c4c
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-12 23:16:07 +0300

    adding dto

---
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/model/dto/StudentRegisterDto.java
@@ -0,0 +1,32 @@
+package org.example.mongodbcourse.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.mongodbcourse.model.entity.Address;
+import org.example.mongodbcourse.model.enums.Gender;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentRegisterDto {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Gender gender;
+
+    private Address address;
+
+    private LocalDateTime createdAt;
+
+    private List<String> favouriteSubjects;
+
+    private BigDecimal totalSpentInBooks;
+}
