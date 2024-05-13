9e2ad493c48480ce38552467d8bbb74e836415f2
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-14 00:06:43 +0300

    renaming class

--- MongoDB-Course/src/main/java/org/example/mongodbcourse/web/StudentController.java
+++ MongoDB-Course/src/main/java/org/example/mongodbcourse/web/StudentController.java
@@ -1,7 +1,7 @@
 package org.example.mongodbcourse.web;
 
 import lombok.RequiredArgsConstructor;
-import org.example.mongodbcourse.model.dto.StudentUpdateDto;
+import org.example.mongodbcourse.model.dto.StudentDto;
 import org.example.mongodbcourse.model.entity.Student;
 import org.example.mongodbcourse.service.StudentService;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +20,12 @@ public class StudentController {
     }
 
     @PostMapping
-    public String addStudent(@RequestBody StudentUpdateDto studentUpdateDto) {
-        return this.studentService.addStudent(studentUpdateDto);
+    public String addStudent(@RequestBody StudentDto studentDto) {
+        return this.studentService.addStudent(studentDto);
     }
 
     @PatchMapping
-    public Student updateStudent(@RequestBody StudentUpdateDto student) {
+    public Student updateStudent(@RequestBody StudentDto student) {
         return this.studentService.updateStudentInfo(student);
     }
 
