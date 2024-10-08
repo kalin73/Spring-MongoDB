cdca2f229c6420d341fcbe7a1b6e0ec946eb721e
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-09 01:25:49 +0300

    validations

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/model/dto/RegisterForm.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/model/dto/RegisterForm.java
@@ -1,5 +1,7 @@
 package com.example.moviesbackend.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,12 @@ import lombok.Setter;
 @Setter
 @Builder
 public class RegisterForm {
+    @Size(min = 3, max = 20)
     private String username;
+
+    @Email
     private String email;
+
+    @Size(min = 3, max = 20)
     private String password;
 }
