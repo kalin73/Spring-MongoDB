3caa1e1fa748841f4e084e42c8fc901a5a3477e7
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-08 18:59:25 +0300

    adding test

---
+++ Movies-project/movies-backend/src/test/java/com/example/moviesbackend/web/rest/ReviewRestControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.moviesbackend.web.rest;
+
+import com.example.moviesbackend.model.dto.ReviewDto;
+import com.example.moviesbackend.service.ReviewService;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class ReviewRestControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ReviewService reviewService;
+
+    private ObjectId objectId;
+
+    @BeforeEach
+    public void setUp() {
+        objectId = ObjectId.get();
+        when(reviewService.createReview(anyString(), anyString())).thenReturn(new ReviewDto(objectId, "Pesho", "Good movie!"));
+    }
+
+    @Test
+    public void testCreateReview() throws Exception {
+        mockMvc.perform(post("/api/v1/reviews")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                          "reviewBody": "Good movie!",
+                          "imdbId": "qwe"
+                        }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.body").value("Good movie!"));
+    }
+}
