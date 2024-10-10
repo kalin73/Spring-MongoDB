69c3f781e0df0d1913867f2ae317d7da07656416
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-10 13:10:10 +0300

    refactoring

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/model/dto/LoginForm.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/model/dto/LoginForm.java
@@ -2,11 +2,15 @@ package com.example.moviesbackend.model.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginForm {
     @Email
     private String email;
