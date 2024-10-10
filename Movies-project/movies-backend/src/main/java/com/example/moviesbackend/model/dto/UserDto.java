ff931e43d4e73be259794f1ba538c9d1dcc7cc4e
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-10 12:21:12 +0300

    refactoring

--- Movies-project/movies-backend/src/main/java/com/example/moviesbackend/model/dto/UserDto.java
+++ Movies-project/movies-backend/src/main/java/com/example/moviesbackend/model/dto/UserDto.java
@@ -1,11 +1,15 @@
 package com.example.moviesbackend.model.dto;
 
 import com.example.moviesbackend.utils.LoggedUser;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private String username;
     private String email;
